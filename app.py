7af9d60f39b9d993d741465b6d810959f5db3c9e
Carlos Jafet Neto <cjafet07@gmail.com>
2025-07-06 21:12:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ Session(app)
 
 # Configure CS50 Library to use SQLite database
 # db = SQL("sqlite:///mempy.db")
-conn = sqlite3.connect("mempy.db")
+conn = sqlite3.connect("/app/mempy.db")
 conn.row_factory = sqlite3.Row  # This enables dictionary-like access
 cursor = conn.cursor()
 
