f6e5752122fc7fdad7f62f118df0f2425c88a5ad
Carlos Jafet Neto <cjafet07@gmail.com>
2025-07-07 19:29:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -446,10 +446,12 @@ def app_settings():
         # Redirect user to login form
         try:
             rows = conn.execute("SELECT * FROM users WHERE username = ?", (user_id,))
+            print("rows", rows)
             user = rows.fetchone()
+            print("user", user)
             user_dict = dict(user)
             print(f"User dict: {user_dict}")
-            return render_template("app-settings.html", api_key=user_dict["api_key"], user_id=user_dict["ser_id"], username=user_dict["username"])
+            return render_template("app-settings.html", api_key=user_dict["api_key"], user_id=user_dict["user_id"], username=user_dict["username"])
         except sqlite3.Error as e:
             print(f"SQLite error: {e}")
             print(f"Error type: {type(e).__name__}")
